0fa68c7b6f21507c660dc585759121345520b5ae
Vince Hehlen <vince.hehlen@gmail.com>
2019-01-21 11:23:42 +0100

    plugin 1st round complete

--- acf-layout-control.php
+++ acf-layout-control.php
@@ -11,3 +11,36 @@
  * Text Domain:  acf-lc-ayout-control
  * Domain Path:  /languages
  */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+	die;
+}
+
+function vh_acf_layout_control() {
+	?>
+	<script type="text/javascript">
+		jQuery(document).ready(function ($) {
+
+			var $buttons = $('<div class="jan_button_bar" style="margin-bottom: 20px;margin-top:-25px;text-align: right"><button class="acf-button button-secondary button acf-layout-control-expand">Module öffnen</button> <button class="acf-button button-secondary button acf-layout-control-colapse">Module schliessen</button></div>');
+			var $target = $('.acf-flexible-content');
+
+			$target.prepend($buttons);
+
+			$('.acf-layout-control-colapse').on('click', function (e) {
+				e.preventDefault();
+				$('.layout').not('.clones .layout').addClass('-collapsed');
+			});
+
+			$('.acf-layout-control-expand').on('click', function (e) {
+				e.preventDefault();
+				$('.layout').not('.clones .layout').removeClass('-collapsed');
+			});
+
+		});
+	</script>
+	<?php
+}
+add_action('admin_footer', 'vh_acf_layout_control');
+
+
