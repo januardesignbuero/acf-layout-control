592ffa5d6e70f46ce5e19fd398fe01f6a37c32a4
Vince Hehlen <vince.hehlen@gmail.com>
2019-01-21 10:24:51 +0100

    init commit

---
+++ acf-layout-control.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:  ACF Layout Control
+ * Plugin URI:
+ * Description:  Adds Open/Close-all-Layouts Buttons to page admin
+ * Version:      0.1.0
+ * Author:       Vince Hehlen
+ * Author URI:   https://github.com/cyberfly999
+ * License:      GPL2
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:  acf-lc-ayout-control
+ * Domain Path:  /languages
+ */
